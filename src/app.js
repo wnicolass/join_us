2f6bca3b0305f98dbdd40a1daf0dd7def826bc7c
wnicolass <nicolaswgl99@gmail.com>
2022-11-08 23:43:19 +0000

    remove useless code :coffin:

--- src/app.js
+++ src/app.js
@@ -1,9 +1,1 @@
-const { faker } = require("@faker-js/faker");
-const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "join_us",
-});
