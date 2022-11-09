1a3aebf6f36d3c712298b0c5e57ad645960ea92a
wnicolass <nicolaswgl99@gmail.com>
2022-11-09 15:15:49 +0000

    run server with express :alembic:

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Welcome to my website");
+  console.log("Someone requested us!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000!");
+});
