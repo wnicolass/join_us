1f6eb83b4b5cd250d7aa69afe1de3fa435955ccc
wnicolass <nicolaswgl99@gmail.com>
2022-11-09 15:35:43 +0000

    write experimental code :alembic:

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ app.get("/", (req, res) => {
   console.log("Someone requested us!");
 });
 
+app.get("/joke", (req, res) => {
+  let joke = "What do you call a dog that does magic tricks? A labracadabrador";
+  res.send(joke);
+  console.log("requested the joke route");
+});
+
+app.get("/random_num", (req, res) => {
+  let randomNum = Math.floor(Math.random() * 10) + 1;
+  res.send("Your lucky number is " + randomNum);
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000!");
 });
