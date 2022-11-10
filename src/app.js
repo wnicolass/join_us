cae82612ea7e685f7c52787ed8aedcf0b0f0c6af
wnicolass <nicolaswgl99@gmail.com>
2022-11-10 19:59:28 +0000

    create public folder and add style.css :lipstick:

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   const myQuery = "SELECT COUNT(*) AS count FROM users";
